e4c350e8cb3359a850fc4c83dd54a585e933c27d
Ikhsan Herdi Fajriyanto <42662876+IkhsanHerdi@users.noreply.github.com>
2018-09-13 09:02:10 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,41 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
+ function rekursif($i){
+  $a = $i;
+  echo "<br>".rek($a);
+  $i++;
+  if($i < 6){
     rekursif($i);
+  }    
 }
-rekursif(5);
-?>
+
+ function rek($p){
+    echo "*";
+    $p++;
+    if ($p < 6){
+        rek ($p);
+    }
+}
+rekursif(1);
+
+echo "<br><br>===============================================================================================================<br><br>";
+
+$batas = 1;
+function rkursif($a = 1){
+    
+    global $batas;
+    echo $a;
+    $a++;
+    if($a<=$batas){
+      rkursif($a);
+    }else {
+        echo "<br>";
+        $batas++;
+        if ($batas<=5) {
+            rkursif();
+        }
+    }
+  }
+  rkursif();
+
+?> 
